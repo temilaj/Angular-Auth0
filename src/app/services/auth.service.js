89f143c79b059b5efa506cccc19bcfb0167cbb0d
Temi Lajumoke <temi@temilajumoke.com>
2017-03-30 22:56:35 +0100

    implement auth service

--- src/app/services/auth.service.js
+++ src/app/services/auth.service.js
@@ -5,15 +5,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
-var AuthService = (function () {
-    function AuthService() {
-        this.lock = new auth0Lock('H6km6ur9OnHghl4bSmgbY4DsNL5WDyfK', 'temilaj.auth0.com', {});
+var angular2_jwt_1 = require("angular2-jwt");
+var Auth = (function () {
+    function Auth() {
+        // Configure Auth0
+        this.lock = new Auth0Lock('H6km6ur9OnHghl4bSmgbY4DsNL5WDyfK', 'temilaj.auth0.com', {});
+        // Add callback for lock `authenticated` event
+        this.lock.on("authenticated", function (authResult) {
+            localStorage.setItem('id_token', authResult.idToken);
+        });
     }
-    return AuthService;
+    Auth.prototype.login = function () {
+        // Call the show method to display the widget.
+        this.lock.show();
+    };
+    Auth.prototype.authenticated = function () {
+        // Check if there's an unexpired JWT
+        // This searches for an item in localStorage with key == 'id_token'
+        return angular2_jwt_1.tokenNotExpired();
+    };
+    Auth.prototype.logout = function () {
+        // Remove token from localStorage
+        localStorage.removeItem('id_token');
+    };
+    return Auth;
 }());
-AuthService = __decorate([
-    core_1.Injectable()
-], AuthService);
-exports.AuthService = AuthService;
+Auth = __decorate([
+    core_1.Injectable(),
+    __metadata("design:paramtypes", [])
+], Auth);
+exports.Auth = Auth;
 //# sourceMappingURL=auth.service.js.map
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,35 @@
-import { Injectable } from '@angular/core';
-import { tokenNotExpired } from 'angular2-jwt';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
 
-declare var auth0Lock : any;
+import { Injectable }      from '@angular/core';
+import { tokenNotExpired } from 'angular2-jwt';
+
+// Avoid name not found warnings
+declare var Auth0Lock: any;
+
 @Injectable()
-export class AuthService {
-    lock = new auth0Lock('H6km6ur9OnHghl4bSmgbY4DsNL5WDyfK', 'temilaj.auth0.com', {})
+export class Auth {
+  // Configure Auth0
+  lock = new Auth0Lock('H6km6ur9OnHghl4bSmgbY4DsNL5WDyfK', 'temilaj.auth0.com', {});
+
+  constructor() {
+    // Add callback for lock `authenticated` event
+    this.lock.on("authenticated", (authResult:any) => {
+      localStorage.setItem('id_token', authResult.idToken);
+    });
+  }
+
+  public login() {
+    // Call the show method to display the widget.
+    this.lock.show();
+  }
+
+  public authenticated() {
+    // Check if there's an unexpired JWT
+    // This searches for an item in localStorage with key == 'id_token'
+    return tokenNotExpired();
+  }
+
+  public logout() {
+    // Remove token from localStorage
+    localStorage.removeItem('id_token');
+  }
 }
